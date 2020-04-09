b6aacf407ca59dbc163ae91aa4324f9443eb1774
darmarius <darmarius123@gmail.com>
2020-04-09 16:14:17 +0300

    email

---
+++ common/email.groovy
@@ -0,0 +1,15 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.mail.Email
+
+StringBuilder body = new StringBuilder()
+body.append("<h1>bla bla bla</h1>")
+
+def mailServer = ComponentAccessor.getMailServerManager().getDefaultSMTPMailServer()
+if (mailServer) {    
+    Email mail = new Email("email1@gmail.com,email2@gmail.com")
+  	mail.setSubject("Subject")
+   	mail.setBody(body.toString())    	
+    mail.setMimeType("text/html")
+   	mailServer.send(mail)
+}   
+
