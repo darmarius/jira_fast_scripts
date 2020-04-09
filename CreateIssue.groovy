da137d67d4184be6e5aabafd4d3da3ae00dfe096
darmarius <darmarius123@gmail.com>
2020-04-09 16:09:01 +0300

    create issue script

---
+++ CreateIssue.groovy
@@ -0,0 +1,20 @@
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.component.ComponentAccessor
+
+ApplicationUser user = ComponentAccessor.getUserManager().getUserByName("Admin")
+def issueFactory = ComponentAccessor.getIssueFactory()
+def issueManager = ComponentAccessor.getIssueManager()
+MutableIssue newTask = issueFactory.getIssue()
+newTask.setSummary("summary")
+newTask.setProjectObject(ComponentAccessor.projectManager.getProjectByCurrentKeyIgnoreCase("TEST"))
+newTask.setDescription("desc")
+newTask.setReporter(user)
+newTask.setAssigneeId("username")
+newTask.setIssueTypeId(ComponentAccessor.getConstantsManager().getAllIssueTypeObjects().find{it.getName() == "Service Request"}.id)
+def cf = ComponentAccessor.customFieldManager.getCustomFieldObjectsByName("CF name").first()
+newTask.setCustomFieldValue(cf,"value")
+
+Map<String,Object> newIssueParams = ["issue" : newTask] as Map<String,Object>
+issueManager.createIssueObject(user, newIssueParams)
+	
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # jira_fast_scripts
+This repo created for fast copy-paste snippets when you writing new script.
