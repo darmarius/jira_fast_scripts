8fdf06c16c4bd921b4200a2b118a232704a792aa
darmarius <darmarius123@gmail.com>
2020-04-09 16:02:53 +0300

    add jql script

---
+++ jql.groovy
@@ -0,0 +1,13 @@
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.bc.issue.search.SearchService
+import com.atlassian.jira.issue.search.SearchResults
+import com.atlassian.jira.web.bean.PagerFilter
+
+ApplicationUser applicationUser = ComponentAccessor.getUserManager().getUserByName("Admin")
+SearchService searchService = ComponentAccessor.getComponentOfType(SearchService.class)
+String jql ="project = test"
+final SearchService.ParseResult parseResult = searchService.parseQuery(applicationUser, jql)
+final SearchResults results = searchService.search(applicationUser, parseResult.getQuery(), PagerFilter.getUnlimitedFilter())
+def issues = results.getResults() // jira < 8.0.0
+//def issues = results.getIssues() // jira > 8.0.0
