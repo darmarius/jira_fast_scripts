c8b1afd1e6c716c252638963c3a7ee1b1df93ee5
darmarius <darmarius123@gmail.com>
2020-04-14 10:50:31 +0300

    update sql select

--- common/selectFromJiraDB.groovy
+++ common/selectFromJiraDB.groovy
@@ -7,20 +7,18 @@ import org.ofbiz.core.entity.DelegatorInterface
 String helperName = ComponentAccessor.getComponent(DelegatorInterface).getGroupHelperName("default")
 StringBuffer sb = new StringBuffer()
 def sqlStmt = """
-  SELECT i.id, CONCAT(p.pkey,'-',issuenum) as issuekey,i.summary,it.pname as issuetype,s.pname as status FROM jiraissue as i 
-  JOIN project as p ON p.id = i.PROJECT
-  JOIN issuetype as it ON it.ID = i.issuetype
-  JOIN issuestatus as s ON s.ID = i.issuestatus
-  WHERE p.pkey = 'TEST' and it.pname in ('New Feature','Task')
-  ORDER BY i.issuenum DESC
+select destination, count(*), count(distinct source) from issuelink where linktype = 10300
+group by destination having count(*) > 1
+
 """
 Connection conn = ConnectionFactory.getConnection(helperName)
 Sql sql = new Sql(conn)
 try {
     sql.eachRow(sqlStmt) {
-        log.info("${it.id} ${it.issuekey} ${it.status}")
+        sb.append("<p>${it}</p>")
     }
 }
 finally {
     sql.close()
 }
+return sb
