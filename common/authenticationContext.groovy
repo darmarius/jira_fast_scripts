d3970d0baad543e757bff968cc00660c244ced86
Bykovski Vladislav <darmarius123@gmail.com>
2020-04-17 15:02:40 +0300

    small fix

--- common/authenticationContext.groovy
+++ common/authenticationContext.groovy
@@ -1,7 +1,6 @@
 import com.atlassian.jira.issue.MutableIssue
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.user.ApplicationUser
-import com.atlassian.jira.component.ComponentAccessor
 
 //get currentUser
 ApplicationUser applicationUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
