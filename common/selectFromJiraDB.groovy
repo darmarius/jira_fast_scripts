94ec02446e76382fc715da97a5f22cd77c1d255b
Bykovski Vladislav <darmarius123@gmail.com>
2020-04-17 15:07:16 +0300

    make as method

--- common/selectFromJiraDB.groovy
+++ common/selectFromJiraDB.groovy
@@ -4,21 +4,26 @@ import java.sql.Connection
 import org.ofbiz.core.entity.ConnectionFactory
 import org.ofbiz.core.entity.DelegatorInterface
 
-String helperName = ComponentAccessor.getComponent(DelegatorInterface).getGroupHelperName("default")
-StringBuffer sb = new StringBuffer()
-def sqlStmt = """
-select destination, count(*), count(distinct source) from issuelink where linktype = 10300
-group by destination having count(*) > 1
-
+String query = """
+SELECT destination, count(*), count(distinct source) 
+FROM issuelink 
+WHERE linktype = 10300
+GROUP BY destination having count(*) > 1
 """
-Connection conn = ConnectionFactory.getConnection(helperName)
-Sql sql = new Sql(conn)
-try {
-    sql.eachRow(sqlStmt) {
-        sb.append("<p>${it}</p>")
+selectFromJiraDB(query)
+
+StringBuffer selectFromJiraDB(String query){
+    String helperName = ComponentAccessor.getComponent(DelegatorInterface).getGroupHelperName("default")
+    StringBuffer sb = new StringBuffer()
+    Connection conn = ConnectionFactory.getConnection(helperName)
+    Sql sql = new Sql(conn)
+    try {
+        sql.eachRow(query) {
+            sb.append("<p>${it}</p>")
+        }
     }
+    finally {
+        sql.close()
+    }
+    return sb
 }
-finally {
-    sql.close()
-}
-return sb
