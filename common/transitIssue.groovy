b15d6da05cc2ebafc4a185ec8d44a1628e6ad13d
Bykovski Vladislav <darmarius123@gmail.com>
2020-04-17 15:08:15 +0300

    remove tabs

--- common/transitIssue.groovy
+++ common/transitIssue.groovy
@@ -8,13 +8,13 @@ import com.atlassian.jira.workflow.TransitionOptions.Builder
 transitIssue(issue,"In progress")
 
 def transitIssue(Issue issue, String transitName){
-	ApplicationUser applicationUser = ComponentAccessor.getUserManager().getUserByKey("admin");
-	def workflow = ComponentAccessor.workflowManager.getWorkflow(issue)
-	int transitionID = workflow.getActionsByName(transitName).first().id
-	IssueService issueService = ComponentAccessor.getIssueService()
-	def builder =  new Builder()
+    ApplicationUser applicationUser = ComponentAccessor.getUserManager().getUserByKey("admin");
+    def workflow = ComponentAccessor.workflowManager.getWorkflow(issue)
+    int transitionID = workflow.getActionsByName(transitName).first().id
+    IssueService issueService = ComponentAccessor.getIssueService()
+    def builder =  new Builder()
     def transopt = builder.skipConditions().skipValidators().skipPermissions()
-	IssueService.TransitionValidationResult transitionValidationResult = issueService.validateTransition(applicationUser, issue.getId(), transitionID, issueService.newIssueInputParameters(),transopt.build())
-	if (transitionValidationResult.isValid()) 
-		IssueService.IssueResult transitionResult = issueService.transition(applicationUser, transitionValidationResult)
+    IssueService.TransitionValidationResult transitionValidationResult = issueService.validateTransition(applicationUser, issue.getId(), transitionID, issueService.newIssueInputParameters(),transopt.build())
+    if (transitionValidationResult.isValid()) 
+	IssueService.IssueResult transitionResult = issueService.transition(applicationUser, transitionValidationResult)
 }
